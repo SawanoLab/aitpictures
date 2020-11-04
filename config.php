81363570f178ada582e63c45bf198579e413e5d0
KihoNakane <x17070xx@aitech.ac.jp>
2020-11-04 17:53:46 +0900

    heroku用にホスト名，DBネーム，ユーザ，パスワードを書き直し

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 
-define('DSN', 'mysql:host=localhost;dbname=aitpicture_test');
-define('DB_USER', 'ipc_user');
-define('DB_PASSWORD', 'sawano-lab');
+// heroku用に書き直し
+define('DSN', 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_ee40734616a27ce');
+define('DB_USER', 'b9eb941608c33b');
+define('DB_PASSWORD', '7718a3be');
